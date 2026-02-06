b3a892122b5eae5513d5af4f00d6f32061e208cd
IsaacPH2005 <parisacahuallpaisaac@gmail.com>
2026-02-06 19:42:54 -0400

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'carrier' => \App\Http\Middleware\CarrierMiddleware::class,
             'inversionista' => \App\Http\Middleware\InvestorMiddleware::class,
-            'inversionista-lease-on' => \App\Http\Middleware\InvestorMiddleware::class,
+            'inversionista-lease-on' => \App\Http\Middleware\InvestorLeaseOnMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
